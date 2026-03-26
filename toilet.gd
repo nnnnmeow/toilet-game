9f0239ba9fce45eafb240f6d03085aeb4dd208fd
TOSTcRa <antostcr@gmail.com>
2026-03-26 17:23:31 +0100

    add spawn_item function, but item is not spawning yet for some reason, will fix this a little bit later

--- toilet.gd
+++ toilet.gd
@@ -1,10 +1,9 @@
 extends StaticBody3D
 
+@onready var main = $"../";
 var flush_amount : int = 10
 var rng = RandomNumberGenerator.new()
 var events_dictionary = {
-	1: "Event",
-	17: "Enemy",
 	100: "Item",
 }
 var categories_dictionary = {
@@ -38,9 +37,20 @@ func try_flushing():
 				drop = events_dictionary[drops]
 				break
 		flush_amount -= 1
+		if drop == "Item":
+			spawn_item();
 		return drop
 	else:
 		return null
 
-func get_item_by_category():
-	return
+func spawn_item():
+	print("spawning item!")
+	var scene = preload("res://scenes/items/whiskey.tscn");
+	var item = scene.instantiate();
+	main.add_child(item);
+	item.position = self.position + Vector3(0, 3, 0);
+	item.scale = Vector3(0.01, 0.01, 0.01);
+	print(item.position)
+	item.apply_impulse(Vector3(0, 10, 0));
+	print(item.visible, " ", item.get_child_count())
+	print(item.scale)
